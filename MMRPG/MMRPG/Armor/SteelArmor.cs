e85ef22162f7e502f00b5466ff44e6a46ecf5f62
Diana-Batenina <dbatenina@mail.ru>
2022-03-16 17:57:38 +0500

    Задание выполнено, вариант 6

---
+++ MMRPG/MMRPG/Armor/SteelArmor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMRPG
+{
+    public class SteelArmor : Armor
+    {
+        public SteelArmor()
+        {
+            Defense = 40;
+            Durability = 25;
+        }
+    }
+}
---
+++ MMRPG/MMRPG/CharacterCreator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMRPG
+{
+    public abstract class CharacterCreator
+    {
+        public abstract Character CreateCharacter(string name);
+    }
+}
